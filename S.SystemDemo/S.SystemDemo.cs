017289ec8e59ffc315ae7bf710e2b36f2aac0bed
heartacker <heartacker@outlook.com>
2023-05-12 00:59:04 +0800

    demo

---
+++ S.SystemDemo/S.SystemDemo.cs
@@ -0,0 +1,21 @@
+﻿using static S.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S.Tests
+{
+    public class SystemDemo
+    {
+        public static void Main(string[] args)
+        {
+            print("hello world");
+            uint[] a = { 1, 2, 3, 4, 5, 6, 7 };
+            print("hello world");
+            print(a);
+            print(a, sep: "\t");
+        }
+    }
+}
