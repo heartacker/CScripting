c9d2263809c7aedde6b15f988021079386b04761
heartacker <heartacker@outlook.com>
2023-05-12 02:03:53 +0800

    add command

--- S.System/System.cs
+++ S.System/System.cs
@@ -5,7 +5,118 @@ using System.Numerics;
 
 namespace S
 {
-
+    /// <summary>
+    /// A:
+    /// abs()
+    /// aiter()
+    /// all()
+    /// any()
+    /// anext()
+    /// ascii()
+    ///
+    /// B:
+    /// bin()    <see cref="bin"/>
+    /// bool()
+    /// breakpoint()
+    /// bytearray()
+    /// bytes()
+    ///
+    /// C:
+    /// callable()
+    /// chr()
+    /// classmethod()
+    /// compile()
+    /// complex()
+    ///
+    /// D:
+    /// delattr()
+    /// dict()
+    /// dir()
+    /// divmod()
+    ///
+    /// E:
+    /// enumerate()
+    /// eval()
+    /// exec()
+    ///
+    /// F:
+    /// filter()
+    /// float()
+    /// format()
+    /// frozenset()
+    ///
+    /// G:
+    /// getattr()
+    /// globals()
+    ///
+    /// H:
+    /// hasattr()
+    /// hash()
+    /// help()
+    /// hex()    <see cref="hex"/>
+    ///
+    /// I:
+    /// id()
+    /// input()
+    /// int()
+    /// isinstance()
+    /// issubclass()
+    /// iter()
+    ///
+    /// L:
+    /// len()
+    /// list()
+    /// locals()
+    ///
+    /// M:
+    /// map()
+    /// max()
+    /// memoryview()
+    /// min()
+    ///
+    /// N:
+    /// next()
+    ///
+    /// O:
+    /// object()
+    /// oct()
+    /// open()
+    /// ord()
+    ///
+    /// P:
+    /// pow()
+    /// print()    <see cref="print"/>
+    /// property()
+    ///
+    /// R:
+    /// range()
+    /// repr()
+    /// reversed()
+    /// round()
+    ///
+    /// S:
+    /// set()
+    /// setattr()
+    /// slice()
+    /// sorted()
+    /// staticmethod()
+    /// str()
+    /// sum()
+    /// super()
+    ///
+    /// T:
+    /// tuple()
+    /// type()
+    ///
+    /// V:
+    /// vars()
+    ///
+    /// Z:
+    /// zip()
+    ///
+    /// _
+    /// __import__()
+    /// </summary>
     public static class System
     {
         /// <summary>
