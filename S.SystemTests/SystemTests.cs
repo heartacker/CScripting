90b7a5d6c0325508dd3a70bfdd64c84bfe209a20
heartacker <heartacker@outlook.com>
2023-05-12 00:58:49 +0800

    工程

---
+++ S.SystemTests/SystemTests.cs
@@ -0,0 +1,48 @@
+﻿using S;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static S.System;
+
+namespace S.Tests
+{
+    [TestClass()]
+    public class SystemTests
+    {
+        [TestMethod()]
+        [DataRow(10, "0xA")]
+        [DataRow(10, "0x0000000A", 8)]
+
+        public void hexTest(int a, string res, int? width = null)
+        {
+            Assert.AreEqual(hex(a, width), res);
+        }
+
+        [TestMethod()]
+        [DataRow(ulong.MaxValue, "0xFFFFFFFFFFFFFFFF")]
+        [DataRow(ulong.MaxValue, "0xFFFFFFFFFFFFFFFF", 8)]
+        public void hexTestUlong(ulong a, string res, int? width = null)
+        {
+            Assert.AreEqual(hex(a, width), res);
+        }
+
+        [TestMethod()]
+        [DataRow(10, "0b1010")]
+        [DataRow(10, "0b00001010", 8)]
+        public void binTest(int a, string res, int? width = null)
+        {
+            Assert.AreEqual(bin(a, width), res);
+        }
+
+        [TestMethod()]
+        [DataRow("hello world")]
+        public void printTest(string abcs)
+        {
+            print(abcs);
+        }
+    }
+}
