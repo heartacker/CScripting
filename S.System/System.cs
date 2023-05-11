492659957fc23e1a90890015873fd584bb2451cc
heartacker <heartacker@outlook.com>
2023-05-11 23:41:08 +0800

    添加项目文件。

---
+++ S.System/System.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace S;
+
+public enum MyEnum
+{
+    a, b, c, d, e, f
+}
+public static class System
+{
+    public static string hex<T>(T num, int? width = null) where T : INumber<T>
+    {
+        if (width == null || width <= 0)
+        {
+            return string.Format("0x{0:X}", num);
+        }
+        else
+        {
+            string format = "0x{0:" + "X" + width + "}";
+            return string.Format(format, num);
+        }
+    }
+}
