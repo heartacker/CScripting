77afa3d6a7fbc7b69009925bd8d1e107beec5dbe
heartacker <heartacker@outlook.com>
2023-05-13 15:59:15 +0800

    bytes

--- Python.System/Python/bytearry.cs
+++ Python.System/Python/bytearry.cs
@@ -10,6 +10,23 @@ namespace Python
     {
         #region bytes
 
+
+        public static byte[] Bytes(string source, string encoding = "utf-8", string errors = "strict")
+                            => bytes(source, encoding, errors);
+        /// <summary>
+        /// 仅支持byte sbyte int uint ushort short 数组
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static byte[] Bytes(IEnumerable<object> source) => bytes(source);
+        public static byte[] Bytes(uint source) => bytes(source);
+        public static byte[] Bytes(int source) => bytes(source);
+
+
+
+
+
+
         public static byte[] bytes(string source, string encoding = "utf-8", string errors = "strict")
                                     => bytearray(source, encoding, errors);
         /// <summary>
@@ -20,6 +37,32 @@ namespace Python
         public static byte[] bytes(IEnumerable<object> source) => bytearray(source);
         public static byte[] bytes(uint source) => bytearray(source);
         public static byte[] bytes(int source) => bytearray(source);
+
+
+
+        #endregion
+
+
+        #region bytearry
+
+
+        public static byte[] Bytearray(string source, string encoding = "utf-8", string errors = "strict")
+            => bytearray(source, encoding, errors);
+
+        public static byte[] Bytearray(uint source) => bytearray(source);
+        public static byte[] Bytearray(int source) => bytearray((uint)source);
+
+        /// <summary>
+        /// 仅支持byte sbyte int uint ushort short 数组
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static byte[] Bytearray(IEnumerable<object> source) => bytearray(source);
+
+
+
+
 
 
         public static byte[] bytearray(string source, string encoding = "utf-8", string errors = "strict")
