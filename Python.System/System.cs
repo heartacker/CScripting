2731d6bc4bbb4885ad57f83350b0fb618690f7e1
heartacker <heartacker@outlook.com>
2023-05-13 15:38:28 +0800

    byte and bytesarray

--- Python.System/System.cs
+++ Python.System/System.cs
@@ -1,5 +1,4 @@
-﻿#define OBSOLETE1
-using System;
+﻿using System;
 using System.Collections;
 using System.IO;
 using System.Linq;
@@ -12,11 +11,6 @@ using static System.Linq.Enumerable;
 using static System.Numerics.Complex;
 using static System.Numerics.BigInteger;
 
-using Python.Runtime;
-using static Python.Runtime.Runtime;
-using static Python.Runtime.Py;
-using static Python.Runtime.PythonEngine;
-
 #if false
 using ObsoleteAttribute = System.ObsoleteAttribute;
 #else
@@ -28,7 +22,7 @@ namespace Python
     /// <summary>
     /// A:
     /// abs()    <see cref="abs"/> <see cref="Math.Abs"/>
-    /// aiter()
+    /// aiter()  // x
     /// all()
     /// any()
     /// anext()
@@ -137,246 +131,7 @@ namespace Python
     /// _
     /// __import__()
     /// </summary>
-    public static class System
+    public static partial class System
     {
-        #region abs
-
-        public static double Abs(Complex num)
-        {
-            return Abs(num.Magnitude);
-        }
-
-        /// <summary>
-        /// 仅支持复数类型
-        /// </summary>
-        /// <param name="num"></param>
-        /// <returns></returns>
-        public static double Abs(PyObject num)
-        {
-            using (GIL())
-            {
-                var c = num.As<Complex>();
-                return Abs(c.Magnitude);
-            }
-        }
-
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `Abs` instead.")]
-#endif
-        public static double abs(Complex num) => Abs(num);
-#if OBSOLETE
-        [Obsolete("This method is deprecated, `using static System.Math;` and use `Abs` instead.")]
-#endif
-        public static int abs(int num) => Math.Abs(num);
-#if OBSOLETE
-        [Obsolete("This method is deprecated, `using static System.Math;` and use `Abs` instead.")]
-#endif
-        public static long abs(long num) => Math.Abs(num);
-#if OBSOLETE
-        [Obsolete("This method is deprecated, `using static System.Math;` and use `Abs` instead.")]
-#endif
-        public static short abs(short num) => Math.Abs(num);
-#if OBSOLETE
-        [Obsolete("This method is deprecated, `using static System.Math;` and use `Abs` instead.")]
-#endif
-        public static sbyte abs(sbyte num) => Math.Abs(num);
-#if OBSOLETE
-        [Obsolete("This method is deprecated, `using static System.Math;` and use `Abs` instead.")]
-#endif
-        public static double abs(double num) => Math.Abs(num);
-#if OBSOLETE
-        [Obsolete("This method is deprecated, `using static System.Math;` and use `Abs` instead.")]
-#endif
-        public static decimal abs(decimal num) => Math.Abs(num);
-#if OBSOLETE
-        [Obsolete("This method is deprecated, `using static System.Math;` and use `Abs` instead.")]
-#endif
-        public static float abs(float num) => Math.Abs(num);
-
-        #endregion
-
-        #region all
-        /// <summary>
-        ///  <see cref="Enumerable"/>
-        /// </summary>
-        /// <typeparam name="TSource"></typeparam>
-        /// <param name="iterable"></param>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
-        public static bool All<TSource>(IEnumerable<TSource> iterable, Func<TSource, bool> predicate = null)
-        {
-            if (predicate == null)
-                return iterable.All(a => Convert.ToBoolean(a));
-            else
-                return iterable.All(predicate);
-        }
-
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `{nameof(All)}` instead.")]
-#endif
-        public static bool all<TSource>(IEnumerable<TSource> iterable, Func<TSource, bool> predicate = null)
-            => All(iterable, predicate);
-
-        #endregion
-
-        #region any
-        /// <summary>
-        ///  <see cref="Enumerable"/>
-        /// </summary>
-        /// <typeparam name="TSource"></typeparam>
-        /// <param name="iterable"></param>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
-        public static bool Any<TSource>(IEnumerable<TSource> iterable, Func<TSource, bool> predicate = null)
-        {
-            if (predicate == null)
-                return iterable.Any();
-            else
-                return iterable.Any(predicate);
-        }
-
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `{nameof(Any)}` instead.")]
-#endif
-        public static bool any<TSource>(IEnumerable<TSource> iterable, Func<TSource, bool> predicate = null)
-            => Any(iterable, predicate);
-
-        #endregion
-
-        #region hex
-        /// <summary>
-        /// 将整形的数转为hex
-        /// </summary>
-        /// <param name="num">整数</param>
-        /// <param name="width">位宽，<see langword="null"/> 则不关注，如果转换结果大于位宽，则取位宽</param>
-        /// <returns>hex字符串</returns>
-        public static string Hex(long num, int? width = null)
-        {
-            if (width == null || width <= 0)
-            {
-                return string.Format("0x{0:X}", num);
-            }
-            else
-            {
-                string format = "0x{0:" + "X" + width + "}";
-                return string.Format(format, num);
-            }
-        }
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `{nameof(Hex)}` instead.")]
-#endif
-        public static string hex(long num, int? width = null) => Hex(num, width);
-
-        /// <summary>
-        /// 将ulong整形的数转为hex
-        /// </summary>
-        /// <param name="num">整数</param>
-        /// <param name="width">位宽，<see langword="null"/> 则不关注，如果转换结果大于位宽，则取位宽</param>
-        /// <returns>hex字符串</returns>
-
-        public static string Hex(ulong num, int? width = null)
-        {
-            if (width == null || width <= 0)
-            {
-                return string.Format("0x{0:X}", num);
-            }
-            else
-            {
-                string format = "0x{0:" + "X" + width + "}";
-                return string.Format(format, num);
-            }
-        }
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `{nameof(Hex)}` instead.")]
-#endif
-        public static string hex(ulong num, int? width = null) => Hex(num, width);
-
-        #endregion
-
-        #region bin
-
-        /// <summary>
-        /// 将整形的数转为二进制
-        /// </summary>
-        /// <param name="num">整数</param>
-        /// <param name="width">位宽，<see langword="null"/> 则不关注，如果转换结果大于位宽，则取位宽</param>
-        /// <returns>二进制字符串</returns>
-        public static string Bin(long num, int? width = null)
-        {
-            if (width == null || width <= 0)
-            {
-                return "0b" + Convert.ToString(num, 2);
-            }
-            else
-            {
-                string format = Convert.ToString(num, 2);
-                if (format.Length < width)
-                {
-                    string par = "";
-                    for (int c = 0; c < width - format.Length; c++)
-                    {
-                        par += "0";
-                    }
-                    return "0b" + par + format;
-                }
-                return "0b" + format;
-            }
-        }
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `{nameof(bin)}` instead.")]
-#endif
-        public static string bin(long num, int? width = null) => Bin(num, width);
-
-        #endregion
-
-        #region print
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `{nameof(Print)}` instead.")]
-#endif
-        public static void print(object objects,
-                                 string sep = " ",
-                                 string end = "\r\n",
-                                 StreamWriter file = null,
-                                 bool flush = false) => Print(objects, sep, end, file, flush);
-
-        public static void Print(object objects,
-                                 string sep = " ",
-                                 string end = "\r\n",
-                                 StreamWriter file = null,
-                                 bool flush = false)
-        {
-            var _file = file ?? Console.Out;
-
-            _file.Write(objects.ToString());
-            _file.Write(end);
-            if (flush) _file.Flush();
-        }
-
-#if OBSOLETE
-        [Obsolete("This method is deprecated, use `{nameof(Print)}` instead.")]
-#endif
-        public static void print(IList objects,
-                                 string sep = " ",
-                                 string end = "\r\n",
-                                 StreamWriter file = null,
-                                 bool flush = false) => Print(objects, sep, end, file, flush);
-
-        public static void Print(IList objects,
-                                 string sep = " ",
-                                 string end = "\r\n",
-                                 StreamWriter file = null,
-                                 bool flush = false)
-        {
-            var _file = file ?? Console.Out;
-            foreach (var item in objects)
-            {
-                _file.Write(item.ToString());
-                _file.Write(sep);
-            }
-            _file.Write(end);
-            if (flush) _file.Flush();
-        }
-        #endregion
-
     }
 }
