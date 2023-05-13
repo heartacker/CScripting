c5a8d8ddc1d159129d34b083405597424a696422
heartacker <heartacker@outlook.com>
2023-05-13 15:59:40 +0800

    hex

---
+++ Python.System/Python/print.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Numerics;
+
+namespace Python
+{
+    public static partial class System
+    {
+        #region print
+
+        public static void Print(object @object,
+                                 string sep = " ",
+                                 string end = "\r\n",
+                                 StreamWriter file = null,
+                                 bool flush = false) => print(@object, sep, end, file, flush);
+
+
+        public static void Print(IList @object,
+                                 string sep = " ",
+                                 string end = "\r\n",
+                                 StreamWriter file = null,
+                                 bool flush = false) => print(@object, sep, end, file, flush);
+
+
+
+
+
+#if OBSOLETE
+        [Obsolete("This method is deprecated, use `{nameof(Print)}` instead.")]
+#endif
+        public static void print(object @object,
+                                 string sep = " ",
+                                 string end = "\r\n",
+                                 StreamWriter file = null,
+                                 bool flush = false)
+        {
+            var _file = file ?? Console.Out;
+
+            _file.Write(@object.ToString());
+            _file.Write(end);
+            if (flush) _file.Flush();
+        }
+
+#if OBSOLETE
+        [Obsolete("This method is deprecated, use `{nameof(Print)}` instead.")]
+#endif
+        public static void print(IList @object,
+                                 string sep = " ",
+                                 string end = "\r\n",
+                                 StreamWriter file = null,
+                                 bool flush = false)
+        {
+            var _file = file ?? Console.Out;
+            foreach (var item in @object)
+            {
+                _file.Write(item.ToString());
+                _file.Write(sep);
+            }
+            _file.Write(end);
+            if (flush) _file.Flush();
+        }
+        #endregion
+
+    }
+}
