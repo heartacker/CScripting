2686db1759793d5a3e660a008fb1843b9a91f292
heartacker <heartacker@outlook.com>
2023-05-14 09:44:27 +0800

    trim the last character

--- Python.System/Python/print.cs
+++ Python.System/Python/print.cs
@@ -16,7 +16,7 @@ namespace Python
                                  bool flush = false) => print(@object, sep, end, file, flush);
 
 
-        public static void Print(IList @object,
+        public static void Print(IEnumerable @object,
                                  string sep = " ",
                                  string end = "\r\n",
                                  StreamWriter file = null,
@@ -45,18 +45,23 @@ namespace Python
 #if OBSOLETE
         [Obsolete("This method is deprecated, use `{nameof(Print)}` instead.")]
 #endif
-        public static void print(IList @object,
+        public static void print(IEnumerable @object,
                                  string sep = " ",
                                  string end = "\r\n",
                                  StreamWriter file = null,
                                  bool flush = false)
         {
             var _file = file ?? Console.Out;
+            string str = "";
             foreach (var item in @object)
             {
-                _file.Write(item.ToString());
-                _file.Write(sep);
+                str += item.ToString();
+                str += sep;
             }
+
+            str = str.Remove(str.Length - sep.Length, sep.Length);
+
+            _file.Write(str);
             _file.Write(end);
             if (flush) _file.Flush();
         }
