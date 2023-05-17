67f156259257037019eee9d2879ebd8c75d4fb54
heartacker <heartacker@outlook.com>
2023-05-17 16:36:07 +0800

    demo

--- CScriptingDemo/CScriptingDemo.cs
+++ CScriptingDemo/CScriptingDemo.cs
@@ -10,14 +10,36 @@
             print(eargs);
 
             print("hello world");
-            uint[] a = { 1, 2, 3, 4, 5, 6, 7 };
+            print("hello world", "\t");// \t no use
+            int[] a = { 1, 2, 3, 4, 5, 6, 7 };
             print(a);
-            print(a, sep: "\t");
+            print(a, sep: "\t"); // \t ok
 
-
+            List<int> b = new()
+            {
+                1,
+                2
+            };
+            print(b);
+            print(b, sep: "\t");
+            Dictionary<string, int> d = new()
+            {
+                { "a", 1 },
+                { "b", 2 }
+            };
+            print(d);
+            print(d, sep: "\t");
+            Dictionary<int, object> d2 = new()
+            {
+                { 1, "a" },
+                { 2, "b" }
+            };
+            print(d2);
+            print(d2, sep: "\t");
 
 
             help();
+            help(d2);
 
             help(a);
             help(typeof(Math));
